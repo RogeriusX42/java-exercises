31587d3ae06a155080cf7e468ec73dbb96aef266
RogeriusX42 <sp.dlrodrigues@gmail.com>
2025-08-04 20:01:49 -0300

    Reactivated Java Skill Tree: Text output Exercise in 7 Languages

---
+++ Exercicios_01_HelloWorld.java
@@ -0,0 +1,11 @@
+public class Exercicios_01_HelloWorld{
+	public static void main(String[] args){
+		System.out.println("English - Hello World!"); 
+		System.out.println("Português - Olá Mundo!"); 
+		System.out.println("Svenska - Hej Världen!"); 
+		System.out.println("Deutsch - Hallo Welt!");
+		System.out.println("Al Arabiya - Marhaban Bialealam"); 
+		System.out.println("Mandarin - Ni Hao Shijie"); 
+	}
+}
+
